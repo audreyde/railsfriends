8d1d7904e71516b60cce5a80ac3f2980baebdf8b
audreyde <audrey@deigaard.org>
2021-06-11 17:45:29 -0500

    tweaked readme file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,6 @@ class HomeController < ApplicationController
   def about
   	@about_me = "My Name is Audrey. Welcome 
   	 my friends app built with Ruby on Rails."
-  	@answer = 2 + 3
   end
   
 end
